a13dfe3a850670662515d42c96080f703e1ff245
jenna-anderson <jenna.d.anderson@gmail.com>
2021-08-25 12:32:19 -0600

    adds form validation for create potluck form if potluck name is empty or already taken

--- client/src/components/CreatePotluck.js
+++ client/src/components/CreatePotluck.js
@@ -18,6 +18,8 @@ const initialFormValues = {
 
 const CreatePotluck = () => {
     const [formValues, setFormValues] = useState(initialFormValues)
+    const [requiredError, setRequiredError] = useState(false)
+    const [takenError, setTakenError] = useState(false)
     const { push } = useHistory()
 
     const handleChange = (e) => {
@@ -29,16 +31,23 @@ const CreatePotluck = () => {
     }
     const handleSubmit = (e) => {
         e.preventDefault()
-        axiosWithAuth()
-        .post('/api/potlucks', formValues)
-        .then(res => {
-            console.log(res.data.potluck_id)
-            push(`/potluck-page/${res.data.potluck_id}`)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-        setFormValues(initialFormValues)
+        if(formValues.potluck_name && formValues.potluck_name !== 0){
+            setRequiredError(false)
+            axiosWithAuth()
+            .post('/api/potlucks', formValues)
+            .then(res => {
+                console.log(res.data.potluck_id)
+                push(`/potluck-page/${res.data.potluck_id}`)
+            })
+            .catch(err => {
+                console.log(err)
+                setTakenError(true)
+            })
+        } else {
+            setTakenError(false)
+            setRequiredError(true)
+        }
+        // setFormValues(initialFormValues)
     }
 
     return(
@@ -48,9 +57,11 @@ const CreatePotluck = () => {
                     <Form.Group as={Col} controlId="formGridEmail" value="potluck.potluck_name" onChange={handleChange}>
                         <Form.Label >Potluck Name</Form.Label>
                         <Form.Control placeholder="Enter potluck name" name="potluck_name"/>
+                        {requiredError ? <Form.Text className="error">Potluck name is required</Form.Text> : null}
+                        {takenError ? <Form.Text className="error">Potluck name is already taken</Form.Text> : null}
                     </Form.Group>
-
                 </Row>
+
                 <Row>
                 <Form.Group as={Col} controlId="formGridDate" value="potluck.potluck_date" onChange={handleChange}>
                     <Form.Label>Date</Form.Label>
@@ -61,14 +72,13 @@ const CreatePotluck = () => {
                     <Form.Label>Time</Form.Label>
                     <Form.Control type="time" name="potluck_time"/>
                 </Form.Group>
-
                 </Row>
 
                 <Form.Group className="mb-3" controlId="formGridAddress2" value="potluck.potluck_location" onChange={handleChange}>
                     <Form.Label>Location</Form.Label>
                     <Form.Control placeholder="Address" name="potluck_location"/>
                 </Form.Group>
-
+                
                 <Button variant="primary" className="button" type="submit" onClick={handleSubmit}>
                     Create
                 </Button>
